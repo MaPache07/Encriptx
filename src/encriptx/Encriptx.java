e1263f396c71f350017f5be269544ade9c163087
MaPache07 <00170517@uca.edu.sv>
2019-08-29 18:53:27 -0600

    Ahora puede desencriptar!! Ademas se corrigieron unos errores.

--- src/encriptx/Encriptx.java
+++ src/encriptx/Encriptx.java
@@ -14,14 +14,40 @@ import java.util.Scanner;
 public class Encriptx {
     
     public static void main(String[] args) {
+        Encriptador encript = new Encriptador();
         Boolean flag = true;
         Scanner input = new Scanner(System.in);
-        String word = "";
-        System.out.println("-------------------------Bienvenido a Encriptx-------------------------");
-        System.out.println("Ingrese la palabra a encriptar.");
-        word = input.nextLine();
-        Encriptador encript = new Encriptador();
-        System.out.println();
-        encript.tran(word);
+        String o = "", word = "", key = "";
+        while(flag){
+            System.out.println("-------------------------Bienvenido a Encriptx-------------------------");
+            System.out.println("1. Encriptar");
+            System.out.println("2. Desencriptar");
+            System.out.println("3. Salir");
+            o = input.nextLine();
+            switch(o){
+                case "1":
+                    System.out.println("-------------------------Encriptar-------------------------");
+                    System.out.println("Ingrese la palabra a encriptar.");
+                    word = input.nextLine();
+                    System.out.println();
+                    encript.encriptar(word);
+                    break;
+                case "2":
+                    System.out.println("-------------------------Encriptar-------------------------");
+                    System.out.println("Ingrese el texto a desencriptar.");
+                    word = input.nextLine();
+                    System.out.println("Ingrese la llave para desencriptar.");
+                    key = input.nextLine();
+                    key = key.toUpperCase();
+                    System.out.println();
+                    encript.desencriptar(word, key);
+                    break;
+                case "3":
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Ingrese una opcion valida.");
+            }
+        }
     }
 }
